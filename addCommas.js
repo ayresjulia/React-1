3569736d554b2974b8e05ad487d819855719f2ba
Julia Ayres <j.bobrineva@gmail.com>
2021-04-08 11:11:17 -0400

    completed addCommas + tests

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,19 @@
-function addCommas() {}
+function addCommas (num) {
+	let numToStr = num.toString();
+	let intNum;
+	let decimals;
+	numToStr.indexOf(".") === -1
+		? (intNum = numToStr.slice(0, numToStr.length))
+		: (intNum = numToStr.slice(0, numToStr.indexOf(".")));
+
+	numToStr.indexOf(".") === -1
+		? (decimals = "")
+		: (decimals = numToStr.slice(numToStr.indexOf(".")));
+
+	let reversedStr = intNum.split("").reverse().join("").toString();
+	let addCommas = reversedStr.match(/.{1,3}/g).join(",");
+	let result = addCommas.split("").reverse().join("");
+	return result + decimals;
+}
 
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -1,7 +1,23 @@
 const addCommas = require("./addCommas");
 
 describe("#addCommas", () => {
-  test("it is a function", () => {
-    expect(typeof addCommas).toBe("function");
-  });
+	test("it is a function", () => {
+		expect(typeof addCommas).toBe("function");
+	});
+	test("it works with positive numbers", () => {
+		expect(addCommas(1234)).toEqual("1,234");
+		expect(addCommas(1000000)).toEqual("1,000,000");
+		expect(addCommas(9876543210)).toEqual("9,876,543,210");
+		expect(addCommas(6)).toEqual("6");
+	});
+	test("it works with negative numbers", () => {
+		expect(addCommas(-10)).toEqual("-10");
+		expect(addCommas(-5678)).toEqual("-5,678");
+	});
+	test("it works with positive decimal numbers", () => {
+		expect(addCommas(12345.678)).toEqual("12,345.678");
+	});
+	test("it works with negative decimal numbers", () => {
+		expect(addCommas(-3141592.65)).toEqual("-3,141,592.65");
+	});
 });
